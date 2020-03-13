86f4fe37f243afa989ee2e9941f22d5c25b859bc
Antonio Ochoa <antonhub00@gmail.com>
2020-03-12 20:24:50 -0600

    Added unapproved functions to students_menu.hs in unit_2

--- unit_2/programs/students_menu.hs
+++ unit_2/programs/students_menu.hs
@@ -1,30 +1,23 @@
-students = [("James", 70.0), ("John", 85.0), ("Robert", 100.0), ("Michael", 75.0),
+students = [("James", 70.0), ("John", 70.0), ("Robert", 100.0), ("Michael", 75.0),
             ("William", 54.0), ("David", 23.0), ("Richard", 0.0), ("Joseph", 14.0)]
 
 
--- Get amount of aproved students
-gaas [] option = 0
-gaas ((name, grade):xs) option
-    | (grade >= 70) = if (option == "approved") then 1 + (gaas xs option)
-                                                else 0 + (gaas xs option)
-    | otherwise = if (option /= "approved") then 1 + (gaas xs option)
-                                                else 0 + (gaas xs option)
+-- Get amount of students (approved or unapproved)
+gas [] option = 0
+gas ((name, grade):xs) option
+    | (grade >= 70) = if (option == "approved") then 1 + (gas xs option)
+                                                else 0 + (gas xs option)
+    | otherwise = if (option /= "approved") then 1 + (gas xs option)
+                                                else 0 + (gas xs option)
 
 
--- Get amount of aproved students
-gaus [] = 0
-gaus ((name, grade):xs)
-    | (grade >= 70) = 1 + (gaus xs)
-    | otherwise = (gaus xs)
-
-
--- Get name of approved students
-gnas [] option = []
-gnas ((name, grade):xs) option
-  | (grade >= 70) = if (option == "approved") then name:(gnas xs option)
-                                              else (gnas xs option)
-  | otherwise = if (option /= "approved") then name:(gnas xs option)
-                                              else (gnas xs option)
+-- Get student names (approved or unapproved)
+gsn [] option = []
+gsn ((name, grade):xs) option
+  | (grade >= 70) = if (option == "approved") then name:(gsn xs option)
+                                              else (gsn xs option)
+  | otherwise = if (option /= "approved") then name:(gsn xs option)
+                                              else (gsn xs option)
 
 
 -- Get average
@@ -36,70 +29,104 @@ ga ((name, grade):xs) (fgrade, num_elements) option
                                               else (ga xs (fgrade, num_elements) option)
 
 
--- Approved students sublist
-ss [] option = []
-ss ((name, grade):xs) option
-    | (grade >= 70) = if (option == "approved") then (name, grade):(ss xs option)
-                                                else (ss xs option)
-    | otherwise = if (option /= "approved") then (name, grade):(ss xs option)
-                                                else (ss xs option)
+-- Get students sublist (approved or unapproved)
+gss [] option = []
+gss ((name, grade):xs) option
+    | (grade >= 70) = if (option == "approved") then (name, grade):(gss xs option)
+                                                else (gss xs option)
+    | otherwise = if (option /= "approved") then (name, grade):(gss xs option)
+                                                else (gss xs option)
 
 
--- call gxgsn with proper params
-gxgsn_prime [] option grade name = name
-gxgsn_prime ((name, grade):xs) option current_grade current_name
-  | (current_grade > grade) = if (option == "approved") then (gxgsn_prime xs option current_grade current_name)
-                                                        else (gxgsn_prime xs option grade name)
-  | otherwise = if (option /= "approved") then (gxgsn_prime xs option current_grade current_name)
-                                                        else (gxgsn_prime xs option grade name)
+-- call gxsn with proper params
+gxsn_prime [] option grade name = name
+gxsn_prime ((name, grade):xs) option current_grade current_name
+  | (current_grade > grade) = if (option == "best") then (gxsn_prime xs option current_grade current_name)
+                                                        else (gxsn_prime xs option grade name)
+  | otherwise = if (option /= "best") then (gxsn_prime xs option current_grade current_name)
+                                                        else (gxsn_prime xs option grade name)
 
 
--- Get the student name with the best or worst grade
-gxgsn [] option = []
-gxgsn ((name, grade):xs) option = gxgsn_prime ((name, grade):xs) option grade name
+-- Get (the best or worst grade) student name
+gxsn [] option = []
+gxsn ((name, grade):xs) option = gxsn_prime ((name, grade):xs) option grade name
 
+
+-- Pass the proper values to gsgf function
+gsgf_prime [] n (freq, ((y_name, y_grade):ys)) = (freq, ys)
+gsgf_prime ((name, grade):xs) n (freq, ((y_name, y_grade):ys))
+  | (n == grade) = (gsgf_prime xs n (freq+1, ((y_name, y_grade):ys)))
+  | otherwise = (gsgf_prime xs n (freq, (((y_name, y_grade):ys) ++ [(name, grade)])))
+
+
+gsgf [] = []
+gsgf ((name, grade):xs) =
+    let
+        (c_freq, new_list) = (gsgf_prime ((name, grade):xs) grade (0,[("",0.0)]))
+     in
+         [(c_freq, grade)] ++ (gsgf new_list)
 
 menu =
     do
         putStrLn "1) Amount of approved students"
-        putStrLn "2) Name of approved students"
-        putStrLn "3) Average of approved students"
-        putStrLn "4) Average of unapproved students"
-        putStrLn "5) Approved students sublist"
-        putStrLn "6) Unapproved students sublist"
-        putStrLn "7) Best grade student name"
-        putStrLn "8) Quit\n"
+        putStrLn "2) Amount of unapproved students"
+        putStrLn "3) Name of approved students"
+        putStrLn "4) Name of unapproved students"
+        putStrLn "5) Average of approved students"
+        putStrLn "6) Average of unapproved students"
+        putStrLn "7) Approved students sublist"
+        putStrLn "8) Unapproved students sublist"
+        putStrLn "9) Best grade student name"
+        putStrLn "10) Worst grade student name"
+        putStrLn "11) Grade frequency"
+        putStrLn "12) Quit\n"
         putStrLn "Type an option:"
 
         choice <- getLine
 
         if (choice == "1") then
             do
-                putStrLn ("\nResult: " ++ (show (gaas students "approved")) ++"\n")
+                putStrLn ("\nResult: " ++ (show (gas students "approved")) ++"\n")
                 menu
         else if (choice == "2") then
             do
-                putStrLn ("\nResult: " ++ (show (gnas students "approved")) ++"\n")
+                putStrLn ("\nResult: " ++ (show (gas students "unapproved")) ++"\n")
                 menu
         else if (choice == "3") then
             do
-                putStrLn ("\nResult: " ++ (show (ga students (0.0, 0.0) "approved")) ++"\n")
+                putStrLn ("\nResult: " ++ (show (gsn students "approved")) ++"\n")
                 menu
         else if (choice == "4") then
             do
-                putStrLn ("\nResult: " ++ (show (ga students (0.0, 0.0) "unapproved")) ++"\n")
+                putStrLn ("\nResult: " ++ (show (gsn students "unapproved")) ++"\n")
                 menu
         else if (choice == "5") then
             do
-                putStrLn ("\nResult: " ++ (show (ss students "approved")) ++"\n")
+                putStrLn ("\nResult: " ++ (show (ga students (0.0, 0.0) "approved")) ++"\n")
                 menu
         else if (choice == "6") then
             do
-                putStrLn ("\nResult: " ++ (show (ss students "unapproved")) ++"\n")
+                putStrLn ("\nResult: " ++ (show (ga students (0.0, 0.0) "unapproved")) ++"\n")
                 menu
         else if (choice == "7") then
             do
-                putStrLn ("\nResult: " ++ (show (gxgsn students "approved")) ++"\n")
+                putStrLn ("\nResult: " ++ (show (gss students "approved")) ++"\n")
+                menu
+        else if (choice == "8") then
+            do
+                putStrLn ("\nResult: " ++ (show (gss students "unapproved")) ++"\n")
+                menu
+        else if (choice == "9") then
+            do
+                putStrLn ("\nResult: " ++ (show (gxsn students "best")) ++"\n")
+                menu
+        else if (choice == "10") then
+            do
+                putStrLn ("\nResult: " ++ (show (gxsn students "worst")) ++"\n")
+                menu
+        else if (choice == "11") then
+            do
+                putStrLn ("\nResult: " ++ (show (gsgf students)) ++"\n")
                 menu
         else
             return ()
